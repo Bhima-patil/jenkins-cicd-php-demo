7c9279ebb98ffa36a6d554758dc8b75859a1c447
Bhima-patil <bhimarayagoudapatil123@gmail.com>
2022-10-14 10:22:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP will! Declarative bbggvbjghgv ok bro how r u pipeline!!</p>'; ?> 
+ <?php echo '<p>Hello world from   PHP will! Declarative bbggvbjghgv ok bro how r u pipeline!!</p>'; ?> 
  </body>
 </html>
